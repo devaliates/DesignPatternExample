105caf996bb750136e6b8f774677f6be56976c47
aliates <devaliates@gmail.com>
2022-01-12 22:56:39 +0300

    temel fonksiyonlar eklendi.

--- Core/Controllers/BaseController.cs
+++ Core/Controllers/BaseController.cs
@@ -14,4 +14,28 @@ public class BaseController<T> where T: class
         Console.WriteLine($"api ye get ({typeof(T).Name}) {id}, isteği yapıldı.");
         return repository.Get(id);
     }
+
+    public IList<T> GetAll()
+    {
+        Console.WriteLine($"api ye GetAll ({typeof(T).Name}), isteği yapıldı.");
+        return repository.GetAll();
+    }
+
+    public void Add(T model)
+    {
+        Console.WriteLine($"api ye Add ({typeof(T).Name}), isteği yapıldı.");
+        repository.Add(model);
+    }
+
+    public void Update(T model)
+    {
+        Console.WriteLine($"api ye Update ({typeof(T).Name}), isteği yapıldı.");
+        repository.Update(model);
+    }
+
+    public void Delete(T model)
+    {
+        Console.WriteLine($"api ye Delete ({typeof(T).Name}), isteği yapıldı.");
+        repository.Delete(model);
+    }
 }
