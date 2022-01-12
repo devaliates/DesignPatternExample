9d2087ca08edaf01e8b14abfbce6edef3121cfab
aliates <devaliates@gmail.com>
2022-01-12 21:58:41 +0300

    added

---
+++ Core/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Controllers;
+
+public class BaseController<T> where T: class
+{
+    private BaseRepository<T> repository;
+
+    public BaseController(BaseRepository<T> repository)
+    {
+        this.repository = repository;
+    }
+
+    public T Get(int id)
+    {
+        Console.WriteLine($"api ye get ({typeof(T).Name}) {id}, isteği yapıldı.");
+        return repository.Get(id);
+    }
+}
